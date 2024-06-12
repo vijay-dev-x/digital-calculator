a0ea85e8edbd0dcd884d81c59f08020df27191a2
vijay-dev-x <vm2218943@gmail.com>
2024-06-12 08:53:56 +0530

    added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const port = 3002;
+const atlasUrl =
+  "mongodb+srv://vtf4592:Rd%401122334455@cluster0.i0swkwz.mongodb.net/lists";
+
+mongoose
+  .connect(atlasUrl)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log("error", err);
+  });
+
+const numScema = new mongoose.Schema({
+  num1: { type: Number, required: true },
+  num2: { type: Number, required: true },
+  sum: { type: Number, required: true },
+});
+
+const numModel = mongoose.model("nums", numScema);
+
 // middleware
 app.use(express.json());
 
@@ -9,6 +30,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+// connect to mongoose--
+
 // Sum route--
 app.get("/sum", (req, res) => {
   const num1 = parseFloat(req.query.num1);
@@ -23,7 +46,7 @@ app.get("/sum", (req, res) => {
   res.send(`The sum of ${num1} and ${num2} is ${sum}`);
 });
 // Sum post route--
-app.post("/sum", (req, res) => {
+app.post("/sum", async (req, res) => {
   const num1 = req.body.num1;
   const num2 = req.body.num2;
 
@@ -38,7 +61,18 @@ app.post("/sum", (req, res) => {
 
   const sum = num1 + num2;
 
-  res.json({ msg: `The sum of ${num1} and ${num2} is ${sum}`, value: sum });
+  const newNum = new numModel({
+    num1: num1,
+    num2: num2,
+    sum: sum,
+  });
+  const saveNum = await newNum.save();
+
+  res.json({
+    msg: `The sum of ${num1} and ${num2} is ${sum}`,
+    value: sum,
+    savedToDB: saveNum,
+  });
 });
 //   listen--
 
